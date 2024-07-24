3a6cc6146385a4d6a830ab3bc17adc9b6b80d4a2
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-24 15:27:41 -0300

    Add: Controller

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,130 @@
+import fs from 'node:fs';
+
+
+const filePath = 'src/dataBase/users.json';
+
+// Función para leer los usuarios desde el archivo
+const readUsersFromFile = () => {
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+  return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+};
+
+// Función para escribir los usuarios en el archivo
+const writeUsersToFile = (users) => {
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2), 'utf-8');
+};
+
+
+export const getUsers = (req, res) => {
+  const dataUsers = readUsersFromFile();
+  res.send(dataUsers);
+};
+
+export const getUser = (req, res) => {
+  const pathUserId = parseInt(req.params.userId, 10);
+
+  if (isNaN(pathUserId)) {
+    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
+  }
+
+  const dataUsers = readUsersFromFile();
+  const userFound = dataUsers.find((user) => user.id === pathUserId);
+
+  userFound ? res.send(userFound) : res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
+}
+
+export const newUser = (req, res) => {
+  let { id, firstName, lastName } = req.body;
+
+  if (!firstName || !lastName || !id) {
+    return res.status(400).json({ message: "Faltan campos requeridos." });
+  }
+
+  id = parseInt(id, 10);
+
+  if (isNaN(id) || id < 1) {
+    return res.status(400).json({ message: "El ID debe ser un número mayor a 0." });
+  }
+
+  if (typeof firstName !== "string" || typeof lastName !== "string") {
+    return res.status(400).json({ message: "El nombre y el apellido deben ser texto." });
+  }
+
+  const dataUsers = readUsersFromFile();
+  if (dataUsers.some((user) => user.id === id)) {
+    return res.status(400).json({ message: "El usuario con este ID ya existe." });
+  }
+
+  const newUser = { id, firstName, lastName };
+  dataUsers.push(newUser);
+  writeUsersToFile(dataUsers);
+
+  res.status(201).json(newUser);
+}
+
+export const updateUser = (req, res) => {
+  const pathUserId = parseInt(req.params.userId, 10);
+
+  if (isNaN(pathUserId)) {
+    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
+  }
+
+  const dataUsers = readUsersFromFile();
+  const userIndex = dataUsers.findIndex((user) => user.id === pathUserId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
+  }
+
+  const { firstName, lastName } = req.body;
+
+  if (!firstName && !lastName) {
+    return res.status(400).json({ message: "Debe proporcionar al menos uno de los campos para actualizar (firstName o lastName)." });
+  }
+
+  let userUpdated = false;
+  const updateMessages = {};
+
+  if (firstName && typeof firstName === "string" && dataUsers[userIndex].firstName !== firstName) {
+    const firstNameAntiguo = dataUsers[userIndex].firstName;
+    updateMessages.firstName = `${firstNameAntiguo} => ${firstName}`;
+    dataUsers[userIndex].firstName = firstName;
+    userUpdated = true;
+  }
+
+  if (lastName && typeof lastName === "string" && dataUsers[userIndex].lastName !== lastName) {
+    const lastNameAntiguo = dataUsers[userIndex].lastName;
+    updateMessages.lastName = `${lastNameAntiguo} => ${lastName}`;
+    dataUsers[userIndex].lastName = lastName;
+    userUpdated = true;
+  }
+
+  if (!userUpdated) {
+    return res.status(304).json({ message: "No se realizaron cambios." });
+  }
+
+  writeUsersToFile(dataUsers);
+  return res.status(200).json({ Modificaciones: updateMessages });
+}
+
+export const deleteUser = (req, res) => {
+  const pathUserId = parseInt(req.params.userId, 10);
+
+  if (isNaN(pathUserId)) {
+    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
+  }
+
+  const dataUsers = readUsersFromFile();
+  const userIndex = dataUsers.findIndex((user) => user.id === pathUserId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
+  }
+
+  const [deletedUser] = dataUsers.splice(userIndex, 1);
+  writeUsersToFile(dataUsers);
+
+  res.status(200).json({ message: `Usuario con id ${pathUserId} eliminado`, deletedUser });
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import fs from 'node:fs';
 
+import { getUsers, newUser, getUser, updateUser, deleteUser} from "../controllers/users.controller.js";
 const router = Router();
 const filePath = 'src/dataBase/users.json';
 
@@ -19,120 +20,18 @@ const writeUsersToFile = (users) => {
 };
 
 // Ruta para obtener todos los usuarios
-router.get('/users', (req, res) => {
-  const dataUsers = readUsersFromFile();
-  res.send(dataUsers);
-});
-
-// Ruta para agregar un nuevo usuario
-router.post('/users/new', (req, res) => {
-  let { id, firstName, lastName } = req.body;
-
-  if (!firstName || !lastName || !id) {
-    return res.status(400).json({ message: "Faltan campos requeridos." });
-  }
-
-  id = parseInt(id, 10);
-
-  if (isNaN(id) || id < 1) {
-    return res.status(400).json({ message: "El ID debe ser un número mayor a 0." });
-  }
-
-  if (typeof firstName !== "string" || typeof lastName !== "string") {
-    return res.status(400).json({ message: "El nombre y el apellido deben ser texto." });
-  }
-
-  const dataUsers = readUsersFromFile();
-  if (dataUsers.some((user) => user.id === id)) {
-    return res.status(400).json({ message: "El usuario con este ID ya existe." });
-  }
-
-  const newUser = { id, firstName, lastName };
-  dataUsers.push(newUser);
-  writeUsersToFile(dataUsers);
-
-  res.status(201).json(newUser);
-});
+router.get('/users', getUsers);
 
 // Ruta para obtener un usuario por ID
-router.get('/users/:userId', (req, res) => {
-  const pathUserId = parseInt(req.params.userId, 10);
+router.get('/users/:userId', getUser);
 
-  if (isNaN(pathUserId)) {
-    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
-  }
-
-  const dataUsers = readUsersFromFile();
-  const userFound = dataUsers.find((user) => user.id === pathUserId);
-
-  userFound ? res.send(userFound) : res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
-});
+// Ruta para agregar un nuevo usuario
+router.post('/users/new', newUser);
 
 // Ruta para actualizar un usuario por ID
-router.put('/users/:userId', (req, res) => {
-  const pathUserId = parseInt(req.params.userId, 10);
-
-  if (isNaN(pathUserId)) {
-    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
-  }
-
-  const dataUsers = readUsersFromFile();
-  const userIndex = dataUsers.findIndex((user) => user.id === pathUserId);
-
-  if (userIndex === -1) {
-    return res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
-  }
-
-  const { firstName, lastName } = req.body;
-
-  if (!firstName && !lastName) {
-    return res.status(400).json({ message: "Debe proporcionar al menos uno de los campos para actualizar (firstName o lastName)." });
-  }
-
-  let userUpdated = false;
-  const updateMessages = {};
-
-  if (firstName && typeof firstName === "string" && dataUsers[userIndex].firstName !== firstName) {
-    const firstNameAntiguo = dataUsers[userIndex].firstName;
-    updateMessages.firstName = `${firstNameAntiguo} => ${firstName}`;
-    dataUsers[userIndex].firstName = firstName;
-    userUpdated = true;
-  }
-
-  if (lastName && typeof lastName === "string" && dataUsers[userIndex].lastName !== lastName) {
-    const lastNameAntiguo = dataUsers[userIndex].lastName;
-    updateMessages.lastName = `${lastNameAntiguo} => ${lastName}`;
-    dataUsers[userIndex].lastName = lastName;
-    userUpdated = true;
-  }
-
-  if (!userUpdated) {
-    return res.status(304).json({ message: "No se realizaron cambios." });
-  }
-
-  writeUsersToFile(dataUsers);
-  return res.status(200).json({ Modificaciones: updateMessages });
-});
+router.put('/users/:userId', updateUser);
 
 // Ruta para eliminar un usuario por ID
-router.delete('/users/:userId', (req, res) => {
-  const pathUserId = parseInt(req.params.userId, 10);
-
-  if (isNaN(pathUserId)) {
-    return res.status(400).json({ message: "ID de usuario invalido. Debe ser un número." });
-  }
-
-  const dataUsers = readUsersFromFile();
-  const userIndex = dataUsers.findIndex((user) => user.id === pathUserId);
-
-  if (userIndex === -1) {
-    return res.status(404).json({ message: `No existe ningun usuario con el id: ${pathUserId}` });
-  }
-
-  const [deletedUser] = dataUsers.splice(userIndex, 1);
-  writeUsersToFile(dataUsers);
-
-  res.status(200).json({ message: `Usuario con id ${pathUserId} eliminado`, deletedUser });
-});
+router.delete('/users/:userId', deleteUser);
 
 export default router;
